e718f32929294edec64ef419aeccc7b8fa733df8
Tess <literature14@gmail.com>
2018-10-21 17:09:38 -0400

    assignment3

---
+++ assignment3/Assignment3java.js
@@ -0,0 +1,12 @@
+document.getElementById("girl").style.visibility="hidden";
+
+document.getElementById("girl").style.visibility="visible";
+
+$('.img_class').hide(); // to hide image
+$('.img_class').show(); // to show image
+
+
+
+$(".girl").click(function(){
+//WHATEVER YOU WANT TO HAPPEN WILL GO HERE.
+})
